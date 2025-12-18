2f54061e310f44468c10da756b9459c1b552b64a
vedant1418 <108506814+vedant1418@users.noreply.github.com>
2025-12-18 06:55:12 +0000

    Fix gitignore and restore email & telegram helpers

--- omkar-main/project/project/settings.py
+++ omkar-main/project/project/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Media files (User uploads)
 MEDIA_URL = '/media/'
